48666c6d00e6e40a7ae06ff3e5df2aea7e850de0
KeshetTadmor <80317440+KeshetTadmor@users.noreply.github.com>
2021-07-01 18:11:23 +0300

    Update test.py

--- neegprd/test.py
+++ neegprd/test.py
@@ -31,25 +31,16 @@ def test_negative_values(df, tag):
         print('the data does not contain negative values')
 
 
-def test_liking_dis(df):
-    """
-    present the distribiution of liking score to see if the liking model is useful for the data
-    Parameters
-    ----------
-    df : data frame
-        data frame containing data from eeg experiment devided by trail.
-    return
-    ----------
-    plot the liking score
-    shows the number of extreme scores
-
-    """
+def test_liking_dis(df, l_tresh=2, h_tresh=5):
     liking = df.liking
     liking_list = []
     for index in range(0, len(liking)):
         liking_list.append(float(liking[index]))
-    print('number of liking above 5: ' + str(len(df.query('liking > 5'))))
-    print('number of liking below 2: ' + str(len(df.query('liking < 2'))))
+    high_tresh = 'liking > ' + str(h_tresh)
+    low_tresh = 'liking < ' + str(l_tresh)
+    print('number of liking above ' + str(h_tresh) +
+          str(len(df.query(high_tresh))))
+    print('number of liking below 2: ' + str(len(df.query(low_tresh))))
     fig, ax = plt.subplots()
     N, bins, patches = ax.hist(liking_list, bins=np.arange(
         0, 7, 0.1), edgecolor='white', linewidth=1)
@@ -57,11 +48,11 @@ def test_liking_dis(df):
     plt.xlabel('liking score')
     plt.ylabel('frequency')
 
-    for i in range(0, 20):
+    for i in range(0, 10*l_tresh):
         patches[i].set_facecolor('darkorange')
-    for i in range(20, 50):
+    for i in range(10*l_tresh, 10*h_tresh):
         patches[i].set_facecolor('gainsboro')
-    for i in range(50, len(patches)):
+    for i in range(10*h_tresh, len(patches)):
         patches[i].set_facecolor('darkorange')
 
     plt.show()
