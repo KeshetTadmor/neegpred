7d58f23ba6670a0535e83534d04556d6c90bdf5d
nimrodlbr <nimrodlbr@gmail.com>
2021-07-01 20:20:13 +0300

    added description to package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='neegpred',
       packages = ['neegpred'],
-      version='0.1',
+      version='0.2',
       description='predicit liking and BDM results from EEG signals',
       url='https://github.com/KeshetTadmor/neegpred', 
       download_url = 'https://github.com/KeshetTadmor/neegpred/archive/refs/tags/0.1.tar.gz', #FILL IN LATER
@@ -10,6 +10,9 @@ setup(name='neegpred',
       author_email='nimrodlbr@gmail.com',
       keywords = ['EEG', 'ML', 'neuro economy'],
       license='GNU General Public License v3.0',
+      long_description='This package is intended to be used by Prof. Dino Levy, It offers a platform to train\n'
+                       'models that can predict the effect of a given commercial by EEG signal processing'
+                       'and Machine learning'
 
       install_requires=['pandas','numpy','mat73','matplotlib','scipy','math','tqdm','seaborn','sklearn','shap','pytest'], 
   
